ad31b249004b5bdad9a3d0b2fc9b09a9f093f6ed
Yerenzter <yerenzter@yandex.com>
2023-03-28 22:51:30 +0800

    Create python.js

---
+++ python.js
@@ -0,0 +1,36 @@
+// PYTHON 3 METHODS JAVASCRIPT.
+
+// Integer Conversion Method.
+function int(y) {
+    return y - [];
+}
+
+// Character Length Method.
+function len(y) {
+    let data = 0;
+    
+    for(r in str(y)) {
+        data += 1;
+    }
+    
+    return data;
+}
+
+// Character Print Method.
+function print(y) {
+    console.log(y);
+}
+
+// String Conversion Method.
+function str(y) {
+    return y + [];
+}
+
+// Usage
+let name = "Yerenzter";
+
+print(name);
+print(len(name))
+print(int(name))
+print(int(10)+int(24))
+print(str(10)+str(24))
